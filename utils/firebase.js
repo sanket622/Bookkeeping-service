ecd8fc3c89ae28d1b503b061788513e431f47298
sanket622 <sanketkumar0068@gmail.com>
2025-04-24 15:02:57 +0530

    updates

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,22 +1,43 @@
 const admin = require('firebase-admin');
-const serviceAccount = require(process.env.FIREBASE_CONFIG);
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+require('dotenv').config();
 
+// Resolve the full path from the environment variable
+const serviceAccountPath = path.resolve(__dirname, '..', process.env.FIREBASE_CONFIG);
+const serviceAccount = require(serviceAccountPath);
+
+// Initialize Firebase
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  storageBucket: "your-project-id.appspot.com"
+  storageBucket: "bookkeeping-95403.appspot.com",
 });
 
 const bucket = admin.storage().bucket();
 
+// Upload function
 const uploadToFirebase = async (file) => {
-  const blob = bucket.file(Date.now() + '-' + file.originalname);
-  const blobStream = blob.createWriteStream({ resumable: false });
+  const fileName = `books/${uuidv4()}${path.extname(file.originalname)}`;
+  const fileRef = bucket.file(fileName);
 
   return new Promise((resolve, reject) => {
-    blobStream.on('finish', async () => {
-      const publicUrl = `https://storage.googleapis.com/${bucket.name}/${blob.name}`;
+    const stream = fileRef.createWriteStream({
+      metadata: {
+        contentType: file.mimetype,
+        metadata: {
+          firebaseStorageDownloadTokens: uuidv4(),
+        },
+      },
+      resumable: false,
+    });
+
+    stream.on('error', (err) => reject(err));
+    stream.on('finish', () => {
+      const publicUrl = `https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/${encodeURIComponent(fileName)}?alt=media`;
       resolve(publicUrl);
-    }).on('error', reject).end(file.buffer);
+    });
+
+    stream.end(file.buffer);
   });
 };
 
